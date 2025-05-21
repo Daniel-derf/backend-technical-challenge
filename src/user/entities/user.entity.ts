0911f2795374cdf85db443160ad37026c558d1c5
Daniel-derf <danielpardinho321@gmail.com>
2025-05-21 15:25:18 -0300

    feat: user entity constructor

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -5,4 +5,22 @@ export class User {
   email: string;
   isActive: boolean;
   profileId: string;
+
+  constructor(input: CreateUser) {
+    this.id = input?.id;
+    this.isActive = input?.isActive || true;
+    this.email = input.email;
+    this.profileId = input.profileId;
+    this.firstName = input.firstName;
+    this.lastName = input.lastName;
+  }
 }
+
+type CreateUser = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  profileId: string;
+  isActive?: boolean;
+  id?: string;
+};
